52d9f12f4824df5ce9456df8e2cbeed93be5e7ff
gdr1461 <maksmatveenko019@gmail.com>
2025-06-30 23:58:56 +0700

    я бебил

--- src/Utils/Loop.py
+++ src/Utils/Loop.py
@@ -22,6 +22,9 @@ class Loop():
         self.thread.start()
 
     def stop(self):
+        if not self.thread or not self.thread.is_alive():
+            return
+
         self.to_stop = True
         self.thread.join()
 
